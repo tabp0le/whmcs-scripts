ae7084d123e26d560050ca5c007000fd71ac6a59
Tab Fitts <tfitts1212@gmail.com>
2020-12-29 11:42:50 -0700

    Backup templates_c before mounting.
    Add some output to the script

--- templates_c_tmpfs/templates_c_tmpfs.sh
+++ templates_c_tmpfs/templates_c_tmpfs.sh
@@ -3,7 +3,9 @@
 TEMPLATES_C_LOCATION="/var/www/vhosts/domain.com"
 TEMPLATES_C_DIR="$TEMPLATES_C_LOCATION/templates_c"
 TEMPLATES_C_PERSIST_DIR="$TEMPLATES_C_LOCATION/templates_c_persist"
+# templates_c/WHMCS directory owner user
 WEBUSER=
+# templates_c/WHMCS directory group
 WEBGROUP=
 WEBUID=$(id -u $WEBUSER)
 WEBGID=$(id -g $WEBUSER)
@@ -49,12 +51,48 @@ EOF
 chown $WEBUSER:$WEBGROUP /usr/lib/systemd/system/whmcs-restore-tpl-cache.service
 chmod 0644 /usr/lib/systemd/system/whmcs-restore-tpl-cache.service
 
+echo "Creating fstab entry..."
+
 cat <<EOF >> /etc/fstab
 tmpfs $TEMPLATES_C_DIR tmpfs defaults,size=1g,noexec,uid=$WEBUID,gid=$WEBGID,mode=0755 0 0
 EOF
 
+echo "fstab entry created"
+echo ""
+echo "Backing up templates_c directory to templates_c_persist..."
+bash -c "/usr/local/sbin/synctemplate_c_cache"
+echo "templates_c backed up"
+echo ""
+echo "Cleaning out templates_c dir to prepare for tmpfs mount..."
 rm -rf $TEMPLATES_C_DIR/*
+echo "templates_c dir cleaned"
+echo ""
+echo "Mounting new templates_c fstab entry to tmpfs..."
 mount -a
+echo "templates_c mounted to tmpfs"
+echo ""
+echo "Restoring templates_c cache from disk to tmpfs dir..."
+bash -c "/usr/local/sbin/restoretemplate_c_cache"
+echo "templates_c cache populated into tmpfs"
 
+echo ""
+echo "Reloading systemd..."
 systemctl daemon-reload
+echo "Systemd reloaded"
+echo "Enabling boot service to restore cache on powerloss or reboot..."
 systemctl enable whmcs-restore-tpl-cache
+echo "Boot service enabled"
+echo ""
+echo "######################"
+echo "          Done        "
+echo "######################"
+echo ""
+echo "##########################################################################"
+echo "Don't forget to create a cronjob to sync your templates_c dir to disk,
+so the boot service can restore the cache in case of power loss.
+
+Create the cronjob under your web user. Something like the following:
+
+*/5 * * * * /usr/local/sbin/synctemplate_c_cache"
+echo "##########################################################################"
+echo ""
